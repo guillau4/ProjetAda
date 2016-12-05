f5bbba8fae5fc322e2248fb80a06dc63fc039f59
guillau4 <laure.guill78@gmail.com>
2016-12-05 22:16:19 +0100

    debut modif

--- liste_generique.adb
+++ liste_generique.adb
@@ -1,38 +1,49 @@
--- liste_generique.ads
----------------------------------------------------------------
+with Ada.Interger_Text_Io; use Ada.Integer_Text_Io;
+with Ada.Text_Io; use Ada.Text_Io;
+with Ada.Unchecked_Deallocation;
 
---   La partie GENERIQUE du package:
---   1. le type Element des elements de la liste
---   2. une procedure "Put" affichant un element
---
-
-generic
-    type Element is private;
-    with procedure Put(E : in Element);
-    
--- Les specifications du package, qui n'utilisent
--- que les elements et procedures generiques
-package Liste_Generique is
-    type Liste is private;
-    type Iterateur is private;
 
     -- Affichage de la liste, dans l'ordre de parcours
-    procedure Affiche_Liste (L : in Liste);
+    procedure Affiche_Liste (L : in Liste) 
+	P : Liste ;   
+	begin
+	   P := L;
+	   while (not Est_Vide(P)) loop
+	      Put(P.Val) ;
+	      P := P.Suiv ;
+	   end loop ;
+	end Affiche;
 
     -- Insertion d'un element V en tete de liste
-    procedure Insere_Tete (V : in Element; L : in out Liste);
+    procedure Insere_Tete (V : in Element; L : in out Liste)
+	begin
+	   L := new Cellule'(V,L) ;
+	end Insere_Tete;
+
 
     -- Vide la liste et libere toute la memoire utilisee
-    procedure Libere_Liste(L : in out Liste);
+    procedure Libere_Liste(L : in out Liste)
+	begin
+		Vide(L);
+	end Libere_Liste;
 
     -- Creation de la liste vide
-    function Creer_Liste return Liste;
+    function Creer_Liste return Liste
+	begin
+		return null;
+	end Creer_Liste;
 
     -- Cree un nouvel iterateur 
-    function Creer_Iterateur (L : Liste) return Iterateur;
+    function Creer_Iterateur (L : Liste) return Iterateur
+	begin
+		return null;
+	end Creer_Iterateur;
+		
 
     -- Liberation d'un iterateur
     procedure Libere_Iterateur(It : in out Iterateur);
+	begin
+	end Libere_Iterateur;
 
     -- Avance d'une case dans la liste
     procedure Suivant(It : in out Iterateur);
@@ -42,16 +53,9 @@ package Liste_Generique is
 
     -- Verifie s'il reste un element a parcourir
     function A_Suivant(It : Iterateur) return Boolean;
+	begin
+		
 
-    FinDeListe : exception;
 
-private 
-    type Cellule;
-    type Liste is access Cellule;
-
-    type Iterateur_Interne;
-    type Iterateur is access Iterateur_Interne;
-   
-end Liste_Generique;
 
 
